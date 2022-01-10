4c1f4264553e12a40366523e9a79e5d39ee5744c
MaxDragonheart <massimilianomoraca@gmail.com>
2022-01-10 13:01:21 +0100

    feat #17 put extensions list into ARG GEOSERVER_VERSION=2.20.1

--- geoserver/scripts/download-extensions.sh
+++ geoserver/scripts/download-extensions.sh
@@ -1,18 +1,40 @@
 #!/usr/bin/env bash
+echo " ---> Extensions for Geoserver's version: $GEOSERVER_VERSION"
 
-echo " ---> Start download extensions"
+declare -a EXTENSIONS_LINK_LIST
+EXTENSIONS_LINK_LIST=(
+  "http://sourceforge.net/projects/geoserver/files/GeoServer/${GEOSERVER_VERSION}/extensions/geoserver-${GEOSERVER_VERSION}-gdal-plugin.zip"
+  "http://sourceforge.net/projects/geoserver/files/GeoServer/${GEOSERVER_VERSION}/extensions/geoserver-${GEOSERVER_VERSION}-jp2k-plugin.zip"
+  "http://sourceforge.net/projects/geoserver/files/GeoServer/${GEOSERVER_VERSION}/extensions/geoserver-${GEOSERVER_VERSION}-monitor-plugin.zip"
+  "http://sourceforge.net/projects/geoserver/files/GeoServer/${GEOSERVER_VERSION}/extensions/geoserver-${GEOSERVER_VERSION}-importer-plugin.zip"
+  "http://sourceforge.net/projects/geoserver/files/GeoServer/${GEOSERVER_VERSION}/extensions/geoserver-${GEOSERVER_VERSION}-querylayer-plugin.zip"
+  "http://sourceforge.net/projects/geoserver/files/GeoServer/${GEOSERVER_VERSION}/extensions/geoserver-${GEOSERVER_VERSION}-vectortiles-plugin.zip"
+  "http://sourceforge.net/projects/geoserver/files/GeoServer/${GEOSERVER_VERSION}/extensions/geoserver-${GEOSERVER_VERSION}-wcs2_0-eo-plugin.zip"
+  "http://sourceforge.net/projects/geoserver/files/GeoServer/${GEOSERVER_VERSION}/extensions/geoserver-${GEOSERVER_VERSION}-wmts-multi-dimensional-plugin.zip"
+  )
+ARRAY_LEN=${#EXTENSIONS_LINK_LIST[@]}
+
+echo " ---> Extention to download: $ARRAY_LEN"
 
 mkdir downloads/
 
-echo " ---> Put extensions list from data/extensions.txt in downloads/"
-wget -i data/extensions.txt -P downloads/
+for url in "${EXTENSIONS_LINK_LIST[@]}";
+do
+  echo " ---> Start download of $url"
+  wget "$url" -P downloads/;
+done
+
+#echo " ---> Start download extensions"
+#echo " ---> Put extensions list from data/extensions.txt in downloads/"
+#wget -i data/extensions.txt -P downloads/
+#rm data/extensions.txt
 
 echo " ---> Unzip downloaded extensions from downloads/ to data/plugins"
-for zipfiles in /downloads/*.zip; do unzip "$zipfiles" '*.jar' -d data/plugins; done
+for zipfiles in /downloads/*.zip;
+do
+  unzip "$zipfiles" '*.jar' -d data/plugins;
+done
 
-#for i in /downloads/*.jar; do cp $i geoserver/geoserver-plugins; done
-
-rm data/extensions.txt
 rm -rf downloads/
 
 echo " ---> Extensions download completed"
